00b98a6bee6ba8bec0b22b068324defa50916993
aungkyawzaww <aungkyawzaww7889@gmail.com>
2024-05-19 00:34:44 +0630

    repair update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const getcontainer = document.querySelector('.container');
 const geticon = document.getElementById('icon');
 const gettextspace = document.querySelector('.textspace');
+const textbody = document.querySelector('.textbody');
+const textspace = document.querySelector('.textspace');
+
 
 
 
@@ -14,17 +17,51 @@ geticon.addEventListener('click',()=>{
     getrec.start();
 
     getrec.addEventListener('result',(e)=>talking(e));
+    // getcontainer.classList.add('active');
 
 });
 
+
+
+let idx = 0;    
 
 function talking(ele){
     // console.log(ele.results[0][0].transcript);
 
     let miceresults = ele.results[0][0].transcript;
+    let textes = miceresults;
+    // textspace.innerText = textes;
 
-    gettextspace.innerText = miceresults;
+    function counttext(){
+        // console.log('hay');
+        // console.log(textes);
+        // console.log(textes.slice(0,idx));
+        textspace.innerText = textes.slice(0,idx);
+
+        idx++
+        if(idx > textes.length){
+            idx = textes.length;
+            // window.location.reload();
+
+        }
+
+       
+        setTimeout(function(){
+            counttext();
+            getcontainer.classList.remove('active');
+
+        },100);
+
+    
+
+    }
+
+    counttext();
 
     getrec.stop();
 
 }
+
+// setTimeout(function(){
+//     window.location.reload();
+// },9000);
